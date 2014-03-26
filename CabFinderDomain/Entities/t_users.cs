f1b1ede7e190330870930915c2d8500ea43a8ab6
Wendy Paz <deirdrak@gmail.com>
2014-03-26 17:38:30 -0600

    nuevas entidades

---
+++ CabFinderDomain/Entities/t_users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabFinderDomain.Entities
+{
+    public class t_users: IEntity
+    {
+        public virtual int id { get; set; }
+        public virtual string name { get; set; }
+        public virtual string password { get; set; }
+        //public virtual string password_salt { get; set; }
+        public virtual DateTime created_at { get; set; }
+        public virtual DateTime updated_at { get; set; }
+        public virtual bool available { get; set; }
+        public virtual int phonenumber { get; set; }
+        public virtual double latitude { get; set; }
+        public virtual double longitude { get; set; }
+        public virtual bool active { get; set; } 
+       
+    }
+}
---
+++ CabFinderWebAPI/Models/t_users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CabFinderWebAPI.Models
+{
+    public class t_users
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        //public virtual string password_salt { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public bool available { get; set; }
+        public int phonenumber { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public bool active { get; set; } 
+    }
+}
